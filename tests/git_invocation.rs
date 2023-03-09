b3fea3b5c9a586b8fcfea3e366d2a32bdba0af36
kklibo <kklibo4@gmail.com>
2023-03-08 17:32:03 -0800

    test_history_subset_squash

--- tests/git_invocation.rs
+++ tests/git_invocation.rs
@@ -107,7 +107,7 @@ fn assert_git_log(log: &str, commit_order: &[usize]) {
 }
 
 #[test]
-fn f() {
+fn test_history_subset_squash() {
     let temp_dir = tempdir().unwrap();
 
     let in_repo_dir = set_up_repo(&temp_dir);
@@ -119,9 +119,26 @@ fn f() {
     let parent_hash = commits.get("commit2").unwrap();
     let section_hash = commits.get("commit4").unwrap();
 
-    in_repo_dir.command_dbg("git", &["branch", "parent", parent_hash]);
-    in_repo_dir.command_dbg("git", &["branch", "section", section_hash]);
-    in_repo_dir.command_dbg("git", &["branch"]);
+    in_repo_dir.command("git", &["branch", "parent", parent_hash]);
+    in_repo_dir.command("git", &["branch", "section", section_hash]);
+    in_repo_dir.command("git", &["branch"]);
+
+    let master_log = in_repo_dir.stdout("git", &["log", "master", "--pretty=format:%s"]);
+    let parent_log = in_repo_dir.stdout("git", &["log", "parent", "--pretty=format:%s"]);
+    let section_log = in_repo_dir.stdout("git", &["log", "section", "--pretty=format:%s"]);
+
+    assert_git_log(&master_log, &[5, 4, 3, 2, 1]);
+    assert_git_log(&parent_log, &[2, 1]);
+    assert_git_log(&section_log, &[4, 3, 2, 1]);
+
+    in_repo_dir.command_dbg("git", &["checkout", "parent"]);
+    in_repo_dir.command_dbg("git", &["merge", "--squash", "--no-commit", "section"]);
+    in_repo_dir.command_dbg("git", &["commit", "-m", "commit6", "--allow-empty"]);
+    assert!(in_repo_dir
+        .stdout("git", &["diff", "parent", "section"])
+        .is_empty());
+
+    in_repo_dir.command_dbg("git", &["rebase", "--onto", "parent", "section", "master"]);
 
     let master_log = in_repo_dir.stdout("git", &["log", "master", "--pretty=format:%s"]);
     let parent_log = in_repo_dir.stdout("git", &["log", "parent", "--pretty=format:%s"]);
@@ -131,11 +148,9 @@ fn f() {
     dbg!(&parent_log);
     dbg!(&section_log);
 
-    assert_git_log(&master_log, &[5, 4, 3, 2, 1]);
-    assert_git_log(&parent_log, &[2, 1]);
+    assert_git_log(&master_log, &[5, 6, 2, 1]);
+    assert_git_log(&parent_log, &[6, 2, 1]);
     assert_git_log(&section_log, &[4, 3, 2, 1]);
-
-    dbg!(commits);
 
     temp_dir.close().unwrap();
 }
